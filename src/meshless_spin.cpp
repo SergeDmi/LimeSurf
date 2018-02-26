7bad00900570a5768b10928e69f6d90e9f0c5ad6
SergeDmi <serge.dmitrieff@gmail.com>
2018-02-26 14:41:21 +0100

    improved readability

--- src/meshless_spin.cpp
+++ src/meshless_spin.cpp
@@ -9,11 +9,26 @@ int main(int argc, char* argv[])
     //std::istringstream fname(argv[1]);
     ifstream config(argv[1]);
     Glossary glos(config);
-    //glos.read_strings(argc-1, argv+1);
-    Part_set_props set_prop(glos);
     Meshless_props simul_prop(glos);
-
+    Part_set_props set_prop(glos);
     Part_set set1(&set_prop);
+    
+    //glos.read_strings(argc-1, argv+1);
+    std::cout << "# Created set1" << std::endl;
+    if (argc>2) {
+        std::cout << "# +++Created set1" << std::endl;
+        std::string fname(argv[2]);
+        set1.create(fname);
+        std::cout << "# Created set1--++++++" << std::endl;
+    }
+    else
+    {
+        std::cout << "# -- Created set1" << std::endl;
+        set1.create();
+        std::cout << "# Created set1-----" << std::endl;
+    }
+
+
     set1.GetStarted();
     double t=0;
     while (t<simul_prop.Tend) {
@@ -24,7 +39,7 @@ int main(int argc, char* argv[])
     }
     std::cout << std::endl;
     set1.ComputeForcesViscous();
-    set1.GetNeighbours();
+    //set1.GetNeighbours();
     set1.Export(0);
     return N;
 }
